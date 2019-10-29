b99f89aab8cf1252a6a546ebc3e715d392d653cd
Clayton Jones <claytonjonesLMP@gmail.com>
2019-10-29 13:49:56 -0700

    Finished populateDeck() function in app.js, it now creates an array of 52 unique cards as objects

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deck of Cards
+
+This is a simple app that creates a deck of cards, and then allows the user to draw and shuffle the deck. The drawn card will be displayed on screen in text (later as an image).
--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+'use strict'
+
 let cardType = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2];
 let cardSuit = ['Diamond', 'Hearts', 'Clubs', 'Spades'];
 let deck = [];
 
 // Create initial deck of careds
-populateDeck = (type, suit) => {
-    let card = {
-        name: '',
-        suit: ''
-    };
-
-    for (let i = 0; i < type.length(); i++) {
-        for (let j = 0; j < suit.length(); j++) {
+function populateDeck(type, suit) {
+    for (let i = 0; i < type.length; i++) {
+        for (let j = 0; j < suit.length; j++) {
+            let card = {};
+            if (typeof type[i] === 'string') {
+                card.value = 10;
+            } else {
+                card.value = type[i];
+            }
+            if (suit[j] === 'Diamond' || suit[j] === 'Hearts') {
+                card.color = 'Red';
+            } else {
+                card.color = 'Black';
+            }
             card.name = type[i];
             card.suit = suit[j];
             deck.push(card);
         }
     }
 }
+
 populateDeck(cardType, cardSuit);
